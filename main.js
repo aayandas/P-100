b93bb3d7053235b8d750a2e950a56ed2b2c17bb9
aayandas <94917206+aayandas@users.noreply.github.com>
2022-03-26 19:24:52 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+
+function start()
+{
+    document.getElementById("textbox").innerHTML= "";
+    recognition.start();
+}
+
+function take_snapshot()
+{
+    console.log(img_id);
+
+    WebCam.snap(function(data_uri) {
+    if(img_id=="selfie1")
+     {
+        document.getElementById("result1").innerHTML = '<img id="selfie1" src= "'+data_uri+ '"/>';
+     }
+     if(img_id=="selfie2")
+     {
+        document.getElementById("result2").innerHTML = '<img id="selfie2" src= "'+data_uri+ '"/>';
+     }
+     if(img_id=="selfie3")
+     {
+        document.getElementById("result3").innerHTML = '<img id="selfie3" src= "'+data_uri+ '"/>';
+     }
+    
+
+    });
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your Selfie in 5 seconds";
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+setTimeout (function()
+{
+    img_id = "selfie1";
+    take_snapshot();
+    speak_data = "Taking your selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 10000);
+
+setTimeout (function()
+{
+    img_id = "selfie2";
+    take_snapshot();
+    speak_data = "Taking your selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 10000);
+
+setTimeout (function()
+{
+    img_id = "selfie3";
+    take_snapshot();
+    speak_data = "Taking your selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 10000);
+
+function save()
+{
+    link= document.getElementById("link");
+    image= document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
